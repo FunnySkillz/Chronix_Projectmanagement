1be5adf8fe3caa7afaeacd849ce7bfaf2d6f7375
FunnySkillz <sebastijan.bogdan@outlook.com>
2025-05-10 14:59:50 +0200

    reset all to 0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,5 @@
-import { defineConfig } from "astro/config";
-
-import react from "@astrojs/react";
-
-// https://astro.build/config
-import tailwind from "@astrojs/tailwind";
-
-const isProd = process.env.NODE_ENV === "production";
+// @ts-check
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  integrations: [react(), tailwind()]
-});
+export default defineConfig({});
